c876028767c563a5d2a0d2306a19a11798e3c568
jefersongad19-bit <jefersongad19@gmail.com>
2026-05-24 23:35:02 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import { useState } from "react"
+
+export default function App() {
+  return <div style={{color:"white",background:"#070c18",height:"100vh",display:"flex",alignItems:"center",justifyContent:"center",fontSize:24}}>FinanceOS carregando...</div>
+}
